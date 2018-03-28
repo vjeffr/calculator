a3e8494ebb5c6319b52dd36f5703e40cde60b26d
Virginia Jeffries <virginia@Virginias-MacBook-Pro.local>
2018-03-28 17:08:59 -0400

    calculator works for one yes for cardiovascular disease and diabetes

--- main.js
+++ main.js
@@ -5,8 +5,15 @@ $(document).ready(function() {
       var yes = $("[value='yes']:checked").length;
       var no = $("[value='no']:checked").length;
 
+
+
+      // Results or PART II of the page
+
+// This is the code for cardiovascular disease
+
       if (yes === 1){
-        console.log("52");
+        $("#cardiovascular").find("span").text("52");
+        // console.log("52");
       } else if (yes === 2){
         console.log("103");
       } else if (yes === 3){
@@ -16,5 +23,21 @@ $(document).ready(function() {
       } else {
         console.log("107");
       }
+
+// This will be the code for Diabetes.
+
+      if (yes === 1){
+        $("#diabetes").find("span").text("17");
+      } else if (yes === 2){
+        console.log("26");
+      } else if (yes === 3){
+        console.log("88");
+      } else if (yes === 4) {
+        console.log("118");
+      } else {
+        console.log("118");
+      }
+
+
   });
 });
