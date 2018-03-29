e5b1445c0daa3298363c43753c63a3bb482711b4
Virginia Jeffries <virginia@Virginias-MacBook-Pro.local>
2018-03-29 13:47:15 -0400

    added js for more ailments

--- main.js
+++ main.js
@@ -13,15 +13,14 @@ $(document).ready(function() {
 
       if (yes === 1){
         $("#cardiovascular").find("span").text("52");
-        // console.log("52");
       } else if (yes === 2){
-        console.log("103");
+        $("#cardiovascular").find("span").text("103");
       } else if (yes === 3){
-        console.log("99");
+        $("#cardiovascular").find("span").text("99");
       } else if (yes === 4) {
-        console.log("107");
+        $("#cardiovascular").find("span").text("107");
       } else {
-        console.log("107");
+        $("#cardiovascular").find("span").text("107");
       }
 
 // This will be the code for Diabetes.
@@ -29,15 +28,56 @@ $(document).ready(function() {
       if (yes === 1){
         $("#diabetes").find("span").text("17");
       } else if (yes === 2){
-        console.log("26");
+        $("#diabetes").find("span").text("33");
       } else if (yes === 3){
-        console.log("88");
+        $("#diabetes").find("span").text("44");
       } else if (yes === 4) {
-        console.log("118");
+        $("#diabetes").find("span").text("67");
       } else {
-        console.log("118");
+        $("#diabetes").find("span").text("67");
       }
 
+// ASTHMA
+
+      if (yes === 1){
+        $("#asthma").find("span").text("10");
+      } else if (yes === 2){
+        $("#asthma").find("span").text("20");
+      } else if (yes === 3){
+        $("#asthma").find("span").text("30");
+      } else if (yes === 4) {
+        $("#asthma").find("span").text("35");
+      } else {
+        $("#asthma").find("span").text("35");
+      }
+
+      // FRACTURES
+
+            if (yes === 1){
+              $("#fracture").find("span").text("15");
+            } else if (yes === 2){
+              $("#fracture").find("span").text("120");
+            } else if (yes === 3){
+              $("#fracture").find("span").text("200");
+            } else if (yes === 4) {
+              $("#fracture").find("span").text("400");
+            } else {
+              $("#fracture").find("span").text("35");
+            }
+
+// DEPRESSION
+
+            if (yes === 1){
+              $("#depression").find("span").text("12");
+            } else if (yes === 2){
+              $("#depression").find("span").text("35");
+            } else if (yes === 3){
+              $("#depression").find("span").text("75");
+            } else if (yes === 4) {
+              $("#depression").find("span").text("110");
+            } else {
+              $("#depression").find("span").text("110");
+            }
 
   });
 });
